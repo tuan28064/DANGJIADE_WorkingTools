bec8d33d701a3d856e1d44f7791c55dc3b6b088c
wangjingtuan <wangjingtuan@qiyi.com>
2017-05-12 14:34:28 +0800

    DANGJIADE_WorkingTools init
    Signed-off-by: wangjingtuan <wangjingtuan@qiyi.com>

---
+++ createDir.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+CURRENT_PATH=$(dirname $0)
+FILE_NAME=$1
+FILE_PATH=$CURRENT_PATH/$1
+
+# 检查命名文件
+if [[ ! -f $FILE_PATH ]]; then
+	echo "###命名文件缺失###"
+	echo "请准备好命名文件"
+	exit -1
+fi
+
+# 创建文件夹
+for name in `cat $FILE_PATH`
+do 
+	#
+	if [[ -d $CURRENT_PATH/$name ]]; then
+		echo "文件夹已存在，无需创建"
+	fi
+
+	# 创建
+	mkdir "$CURRENT_PATH/$name"
+
+	#
+	if [[ ! -d $CURRENT_PATH/$name ]]; then
+		echo "创建文件夹失败"
+		exit -1
+	fi
+done
+
+echo "创建完毕~"
